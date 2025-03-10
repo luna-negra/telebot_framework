f2bdd4c74ee73cc5a02c2ab352bc2dfd2ea8ed9e
pavel <blitzangreiffer@gmail.com>
2025-03-10 17:16:54 +0900

    telebot_framework_v_0.0: 2025.03.10 - Add Feature for Receiving Location
    [ New ]
    - User can send his or her location to the bot.
    - bot will postprocessing user's location information with longitude and latitude
    
    [ To Be ]
    - Add feature for update location in real time
    - send location from bot to telegram user
    - send location which telegram user want to search with name of area.

--- core/handlers/geo/__init__.py
+++ core/handlers/geo/__init__.py
@@ -1,5 +1,55 @@
-from core.handlers.handlers import ReceiverBasic
+from core.handlers.handlers import ResultShowingWithInlineMarkup
+from core.handlers.handlers import SenderWithBasic
 
 
-class ReceiverWithGeoInfo(ReceiverBasic):
+class ReceiverWithLocation(ResultShowingWithInlineMarkup):
+    """
+    ReceiverWithLocation:
+
+    this class is responsible for sending location of telegram user.
+    when user sends its location on Attachment > location, telegram can grap the location information(latitude and logitude).
+
+    this class can be used to update current location or record travel routes and so on.
+    user must send its location manually.
+    """
+
+    def __init__(self, types, **kwargs):
+        super(ReceiverWithLocation, self).__init__(types, **kwargs)
+        self.location = None
+
+
+    async def get_location(self) -> bool:
+        """
+        this method is charge of sending preamble message and receiving user's current location.
+        you can reprocess the location information with self.location:dict
+
+        :return: bool
+        """
+
+        if getattr(self.types, "location", None) is not None:
+            self.location = self.types.location.to_dict()
+            await self.post_process()
+
+            await self.bot.send_message(chat_id=self.chat_id,
+                                        text=self.bot_text)
+            return True
+
+        await super().send_message()
+        return False
+
+
+class ReceiverWithRTLocation:
+    # designing from 2025.03.10...
+    pass
+
+
+class SenderWithLocation(SenderWithBasic):
+    # send location info with longitude and latitude
+    # designing from 2025.03.10...
+    pass
+
+
+class SenderWithSearchedLocation(SenderWithLocation):
+    # send location information that user want to search the name of site or area.
+    # designing from 2025.03.10
     pass
