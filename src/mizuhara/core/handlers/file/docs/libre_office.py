4a09fc93db0f4a28942abf065e25f03ef8f2470b
pavel <blitzangreiffer@gmail.com>
2025-03-19 09:53:37 +0900

    telebot_framwork: 2025.03.19 - Add comments
    - Add comments for new classes and methods

--- src/mizuhara/core/handlers/file/docs/libre_office.py
+++ src/mizuhara/core/handlers/file/docs/libre_office.py
@@ -2,17 +2,13 @@ from mizuhara.core.handlers.file.docs import ReceiverWithDocs
 from mizuhara.translation import translate
 
 
-class ReceiverWithWriterFile(ReceiverWithDocs):
-    async def validate_file(self) -> None:
-        if not self.file_type == "application/vnd.oasis.opendocument.text":
-            raise ValueError(translate(domain="default_warnings",
-                                       key="warn_upload_not_libre_writer",
-                                       language_code=self.language))
-
-        return None
-
-
 class ReceiverWithCalcFile(ReceiverWithDocs):
+    """
+    ReceiverWithCalcFile:
+
+    this class is charge of receiving Libre Office Calc file from telegram user.
+    """
+
     async def validate_file(self):
         if not self.file_type == "application/vnd.oasis.opendocument.spreadsheet":
             raise ValueError(translate(domain="default_warnings",
@@ -23,10 +19,32 @@ class ReceiverWithCalcFile(ReceiverWithDocs):
 
 
 class ReceiverWithImpressFile(ReceiverWithDocs):
+    """
+    ReceiverWithImpressFile:
+
+    this class is charge of receiving Libre Office Impress file from telegram user.
+    """
+
     async def validate_file(self):
         if not self.file_type == "application/vnd.oasis.opendocument.presentation":
             raise ValueError(translate(domain="default_warnings",
                                        key="warn_upload_not_libre_impress",
+                                       language_code=self.language))
+
+        return None
+
+
+class ReceiverWithWriterFile(ReceiverWithDocs):
+    """
+    ReceiverWithWriterFile:
+
+    this class is charge of receiving Libre Office Writer file from telegram user.
+    """
+
+    async def validate_file(self) -> None:
+        if not self.file_type == "application/vnd.oasis.opendocument.text":
+            raise ValueError(translate(domain="default_warnings",
+                                       key="warn_upload_not_libre_writer",
                                        language_code=self.language))
 
         return None
--- src/mizuhara/core/handlers/file/docs/microsoft_office.py
+++ src/mizuhara/core/handlers/file/docs/microsoft_office.py
@@ -3,6 +3,12 @@ from mizuhara.translation import translate
 
 
 class ReceiverWithExcelFile(ReceiverWithDocs):
+    """
+    ReceiverWithExcelFile
+
+    this class is charge of receiving MS Office Excel file from telegram user.
+    """
+
     async def validate_file(self):
         if not self.file_type == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":
             raise ValueError(translate(domain="default_warnings",
@@ -13,6 +19,12 @@ class ReceiverWithExcelFile(ReceiverWithDocs):
 
 
 class ReceiverWithPPTFile(ReceiverWithDocs):
+    """
+    ReceiverWithPPTFile
+
+    this class is charge of receiving MS Office PPT file from telegram user.
+    """
+
     async def validate_file(self):
         if not self.file_type == "application/vnd.openxmlformats-officedocument.presentationml.presentation":
             raise ValueError(translate(domain="default_warnings",
@@ -21,6 +33,12 @@ class ReceiverWithPPTFile(ReceiverWithDocs):
 
 
 class ReceiverWithWordFile(ReceiverWithDocs):
+    """
+    ReceiverWithWordFile
+
+    this class is charge of receiving MS Office Word file from telegram user.
+    """
+
     async def validate_file(self):
         if not self.file_type == "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
             raise ValueError(translate(domain="default_warnings",
