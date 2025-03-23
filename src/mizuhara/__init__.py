d3f5c68191ba120a8868871894030f75a2fb3fb7
pavel <blitzangreiffer@gmail.com>
2025-03-23 12:18:00 +0900

    telebot_framework_v_0.0: 2025.03.23 - Edit core/__init__.py
    -  Debugging on mizuhara command.

--- src/mizuhara/__init__.py
+++ src/mizuhara/__init__.py
@@ -1,4 +1,9 @@
 from .config_loader import load_config
 
 # Load config when `mizuhara` is imported
-config = load_config()
+try :
+    config = load_config()
+
+# add exception handler to avoid exception during creating project or app
+except FileNotFoundError:
+    pass
